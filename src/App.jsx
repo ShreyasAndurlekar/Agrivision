527b6ab4af5826de8eb9c566c4bbfdbf0f9b6d4c
ShreyasAndurlekar <and.shreyas@gmail.com>
2025-01-26 20:52:01 +0530

    Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Welcome from "./pages/Welcome";
 import Disease from "./pages/Disease";
-import Croppred from "./pages/Croppred"
+import Croppred from "./pages/Croppred";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
   return (
@@ -9,8 +13,11 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/disease" element={<Disease />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/Croppred" element={<Croppred />} />
       </Routes>
+      <ToastContainer />
     </Router>
   );
 };
